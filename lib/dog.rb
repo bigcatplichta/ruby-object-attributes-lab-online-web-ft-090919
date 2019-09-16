e7e4f0eb04df48563244e11969c2024bad08385d
Matt Plichta <bigcatplichta@gmail.com>
2019-09-16 20:02:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,13 @@ class Dog
   def name=(dog_name)  
     @name = dog_name
   end 
+  def name 
+    @name 
+  end 
   def breed=(breed)
     @breed = breed 
+  end 
+  def breed 
+    @breed 
   end 
 end 
